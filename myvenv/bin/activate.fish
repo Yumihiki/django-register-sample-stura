486ddd1c56b4b80f70a9c580356bf61952fec2bb
ゲストユーザ <Guest@MBP-Osaka01.local>
2020-02-26 10:52:32 +0900

    Djangoで、ユーザー情報閲覧・更新ページの作成　終了

--- myvenv/bin/activate.fish
+++ myvenv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/Guest/register/myvenv"
+set -gx VIRTUAL_ENV "/Users/Guest/stura/django-register-sample-stura/myvenv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
